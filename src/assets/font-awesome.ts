89eed5fdca3c0388815a289bfa53e5be2dcfb498
Lola Rufino <lola.ruf.ar@gmail.com>
2021-11-02 18:10:17 +0100

    Add more icons to font awesome

--- src/assets/font-awesome.ts
+++ src/assets/font-awesome.ts
@@ -1,10 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {faMugHot, faShoppingBasket, faList, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
+import {faMugHot, faShoppingBasket, faList, faShoppingCart, faPlus, faMinus} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faMugHot);
 library.add(faList);
 library.add(faShoppingBasket);
 library.add(faShoppingCart);
+library.add(faPlus);
+library.add(faMinus);
 
 export default FontAwesomeIcon;
