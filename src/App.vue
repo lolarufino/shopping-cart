279877685464fd0d083647645fef647c06f11878
Lola Rufino <lola.ruf.ar@gmail.com>
2021-11-02 12:06:31 +0100

    Delete default values on app and use nav as component and router view

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <Nav />
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import Nav from "../src/components/Nav.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Nav,
+  },
+});
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+a {
+  text-decoration: none;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: space-between;
 }
 </style>
