2341ecebce69973aaa044e7110261164d2b908e9
Lola Rufino <lola.ruf.ar@gmail.com>
2021-11-02 12:12:52 +0100

    Create Home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <p class="text">
+    At Tea Shop we have been dedicated body and soul to the world of tea since
+    1990. Discover in our online tea shop the best selection of online tea where
+    you will find Teas, Rooibos, Infusions that will make you fall in love.
+  </p>
+  <img
+    class="home-image"
+    src="https://i.ibb.co/kMM9DjP/animation-500-kvgkn43t.gif"
+    alt="Teapot gif"
+  />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+<style scoped lang="scss">
+@import "../styles/variables.scss";
+.text {
+  font-family: $bodyfont;
+  font-size: 20px;
+  text-align: center;
+  padding: 60px;
+}
+.home-image {
+  width: 450px;
+  align-self: center;
+}
+</style>
