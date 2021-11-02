3d783d54df0131af74bca3953d2d87df53c8ee30
Lola Rufino <lola.ruf.ar@gmail.com>
2021-11-02 12:07:05 +0100

    Configure font awesome library

---
+++ src/assets/font-awesome.ts
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {faMugHot, faShoppingBasket, faList, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faMugHot);
+library.add(faList);
+library.add(faShoppingBasket);
+library.add(faShoppingCart);
+
+export default FontAwesomeIcon;
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import FontAwesomeIcon from "../src/assets/font-awesome";
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(store).use(router).mount('#app');
