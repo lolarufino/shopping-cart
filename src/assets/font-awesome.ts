e3a8c430d658b80fc5ddb6d8175757e2b227c92f
Lola Rufino <lola.ruf.ar@gmail.com>
2021-11-03 16:12:13 +0100

    Add trash icon from fontawesome

--- src/assets/font-awesome.ts
+++ src/assets/font-awesome.ts
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {faMugHot, faShoppingBasket, faList, faShoppingCart, faPlus, faMinus} from "@fortawesome/free-solid-svg-icons";
+import {faMugHot, faShoppingBasket, faList, faShoppingCart, faPlus, faMinus, faTrashAlt} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faMugHot);
 library.add(faList);
@@ -8,5 +8,6 @@ library.add(faShoppingBasket);
 library.add(faShoppingCart);
 library.add(faPlus);
 library.add(faMinus);
+library.add(faTrashAlt);
 
 export default FontAwesomeIcon;
