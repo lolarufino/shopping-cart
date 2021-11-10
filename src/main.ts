e2ae9fa16934357d5933be16a1d7cef178a52c70
Lola Rufino <lola.ruf.ar@gmail.com>
2021-11-10 12:24:45 +0100

    Configure prettier to ident typescript files

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 import FontAwesomeIcon from "../src/assets/font-awesome";
 import Toast, { POSITION } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 const options = {
-    position: POSITION.BOTTOM_CENTER,
-    timeout: 2000,
-}
+  position: POSITION.BOTTOM_CENTER,
+  timeout: 2000,
+};
 
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(store).use(router).use(Toast, options).mount('#app');
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(store)
+  .use(router)
+  .use(Toast, options)
+  .mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,24 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
-import Products from '../views/Products.vue';
-import Cart from '../views/Cart.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+import Products from "../views/Products.vue";
+import Cart from "../views/Cart.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/products',
-    name: 'Products',
+    path: "/products",
+    name: "Products",
     component: Products,
   },
   {
-    path: '/cart',
-    name: 'Cart',
+    path: "/cart",
+    name: "Cart",
     component: Cart,
   },
-  
 ];
 
 const router = createRouter({
