411e6b6e86cf3b070f5fea83d7e57fd5f63f3fde
Lola Rufino <lola.ruf.ar@gmail.com>
2021-11-10 12:29:59 +0100

    Ident properly with prettier typescript file of interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,13 +1,13 @@
 export interface State {
-    products: Array<Product>
-    productsInCart: Array<Product>
+  products: Array<Product>;
+  productsInCart: Array<Product>;
 }
 
 export interface Product {
-    id: number,
-    image: string,
-    name: string,
-    price: number,
-    amount: number,
-    stock: number 
+  id: number;
+  image: string;
+  name: string;
+  price: number;
+  amount: number;
+  stock: number;
 }
